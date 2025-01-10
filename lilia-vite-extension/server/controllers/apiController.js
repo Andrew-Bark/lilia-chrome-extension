49e5fa22b5a2d8fcde89e20b3ef0720cebf66311
Andrew-Bark <gingerupert@gmail.com>
2025-01-10 14:38:34 +0100

    testing dotenv

--- lilia-vite-extension/server/controllers/apiController.js
+++ lilia-vite-extension/server/controllers/apiController.js
@@ -1,11 +1,14 @@
 'use strict';
-
+async function promptOpenAI (wordData){
+    console.log("wordData", wordData)
+    
+}
 module.exports =  {
     async postMessage(req, res){
         try {
             
-            console.log("body", req.body)
-            res.send('Hello from the server');
+            promptOpenAI(req.body)
+            res.send({"success": true});
         } catch (error) {
             console.error("controller get error", error)
             res.status(500).json({ message: "Internal server error" });
